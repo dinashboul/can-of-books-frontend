0eba7e2691c62086b6abb065abb56227d03400cc
dinashboul <dinaalshboul@gmail.com>
2022-09-04 23:53:33 +0300

    lab11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -10,22 +11,48 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3001/books`)
+    .then(result =>{
+      console.log(result.data);
+      this.setState({
+        books : result.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+    
+  }
   render() {
 
     /* TODO: render all the books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
+    return(
+      <div >
+        <h1>Book System</h1>
+        <Carousel slide={true}>
+        {this.state.books.map(item =>{
+          return(
+              <div style={{ width: "400px" , height :"10px"}}>
+           
+               
+               <Carousel.Item interval={500}>
+        <Carousel.Caption>
+        <h3> Books title:{item.title}</h3>
+            <p>{item.description}</p>
+             <p>{item.status}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    
+        
+      </div>
+          )
+        })}
+          </Carousel>
+      </div>
     )
+      }
   }
-}
 
 export default BestBooks;
